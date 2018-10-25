24ef9d4338c167490ae40769eeedc8a25cdde66e
Bertrand-Pelletier <bertrand-888@hotmail.fr>
2018-10-25 20:06:17 +0200

    ajout_airbnb

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+#5.times do 
+#	city = City.create(city_name: Faker::Address.city)
+#end
+
+
+#10.times do 
+#	dog = Dog.create(name: Faker::Dog.name,city_id: rand((City.first.id)..(City.last.id)))
+#end
+#10.times do 
+ #   dog = Dogsitter.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city_id: rand((City.first.id)..(City.last.id)))
+#end
+
+10.times do 
+    stroll = Stroll.create!(date: Faker::Time.forward(7, :day), city_id: rand((City.first.id)..(City.last.id)), dogsitter_id: rand((Dogsitter.first.id)..(Dogsitter.last.id)), dog_id: rand((Dog.first.id)..(Dog.last.id)))
+    stroll.dogsitter_id = Dogsitter.where(city_id: stroll.city_id).pluck(:id).sample
+end
